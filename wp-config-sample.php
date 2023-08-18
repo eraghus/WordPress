8abaed61f4ee99a2e4b3ebe3209e0468d97e568d
eraghus <142589458+eraghus@users.noreply.github.com>
2023-08-18 15:38:11 +0100

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -83,7 +83,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('WP_SITEURL', 'https://blog.digimanifest.store');
+define('WP_HOME', 'https://blog.digimanifest.store');
 
 /* That's all, stop editing! Happy publishing. */
 
