68d010a50e0c006ff511773948802e440a2d1385
eraghus <142589458+eraghus@users.noreply.github.com>
2023-08-19 01:43:16 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'heroku_95c1b5b3a124329' );
+define( 'DB_NAME', 'd7424vrhrq44cod' );
 
 /** Database username */
-define( 'DB_USER', 'b53ae12976b71a' );
+define( 'DB_USER', 'uhujq5pvq2wilbx' );
 
 /** Database password */
-define( 'DB_PASSWORD', '194846c3' );
+define( 'DB_PASSWORD', 'h75YjKR*s#yB9rRb5G9z8OGCT' );
 
 /** Database hostname */
-define( 'DB_HOST', 'us-cdbr-east-06.cleardb.net' );
+define( 'DB_HOST', ' eu-az-sql-serv1.database.windows.net' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
